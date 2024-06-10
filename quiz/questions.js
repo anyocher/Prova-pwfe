9e4bba401cb4e0d33ef23599a2b9a759359588f0
mirellaxsj <mirellafrancasenai@gmail.com>
2024-06-10 10:53:13 -0300

    add

---
+++ quiz/questions.js
@@ -0,0 +1,98 @@
+export default [
+    {
+        question: "Quando foi criado o primeiro computador",
+        answers: [
+            {option: "1943 á 1946", correct:true},
+            {option: "1942 á 1940", correct:false},
+            {option: "1945 á 1945", correct:false},
+        ],
+    },
+
+    {
+        question: "Qual era o nome do primeiro modelo de computador pessoal?",
+        answers: [
+            {option: "IPS 263 PC", correct:false},
+            {option: "IBM 5051 PC", correct:true},
+            {option: "IPIC 9000 PC", correct:false},
+        ],
+    },
+    
+    {
+        question: "Qual foi o motivo da criação do primeiro computador?",
+        answers: [
+            {option: "Para entretenimento da época", correct:false},
+            {option: "Uma melhoria da maquina de escrever", correct:false},
+            {option: "a intenção da máquina era a de realizar cálculos melhor do que um humano", correct:true},
+        ],
+        
+    },
+
+    {
+        question: " Qual é a origem da palavra “computador”?",
+        answers: [
+            {option: "computar", correct:true},
+            {option: "Logar", correct:false},
+            {option: "Rodar", correct:false},
+        ],
+        
+    },
+
+    {
+        question: " Qual foi a contribuição de Joseph Jacquard?",
+        answers: [
+            {option: "Um teclado automatizado", correct:false},
+            {option: "um tear inteiramente automatizado", correct:true},
+            {option: "mouse", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "Porque Charles Babagge é conhecido como o pai do computador?",
+        answers: [
+            {option: "por ter criado o primeiro computador programável do mundo", correct:true},
+            {option: "por ter feito a placa mãe do computador", correct:false},
+            {option: "por ser dono da empresa que foi feito o computador", correct:false},
+        ],
+        
+    },
+    {
+        question: ")Qual foi a primeira linguagem de programação?",
+        answers: [
+            {option: "Lisp", correct:true},
+            {option: "Cobol", correct:false},
+            {option: "Fortran", correct:true},
+        ],
+        
+    },
+
+    {
+        question: "Qual é a origem da expressão “bugs”",
+        answers: [
+            {option: "surgimento de “erro” ", correct:true},
+            {option: "surgimento de “erro de internet” ", correct:false},
+            {option: "surgimento de “began” ", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "O que é um supercomputador?",
+        answers: [
+            {option: "computador grande", correct:false},
+            {option: "computador de grande potência e velocidade", correct:true},
+            {option: "Computador de simulação", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "O que é um Computador? ",
+        answers: [
+            {option: " máquina destinada ao processamento de dados ", correct:true},
+            {option: " máquina para entreterimento", correct:false},
+            {option: " máquina de cores", correct:false},
+        ],
+        
+    },
+];
